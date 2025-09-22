3b8a637e93de535a87f21b0e3d53f143d0e3e173
Adison Chire <adison.chire.broncano@gmail.com>
2025-09-22 00:32:04 -0500

    assets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,7 +20,7 @@ export const projects = [
   {
     id: 2,
     title: "Barbershop Data Analytics",
-    description: "Sistema de análisis datos para una barbería mediante SQL Server y Power BI. El sistema centraliza información de clientes, empleados, inventario, servicios y ventas, permitiendo generar reportes interactivos que optimizan la toma de decisiones, reducen errores y mejoran la atención al cliente.",
+    description: "Proyecto de análisis de datos para una barbería, enfocado en tableros dinámicos con métricas de ventas, clientes, productos e inventario, además de evaluar el rendimiento del personal y permitir una exploración detallada de las transacciones para mejorar la toma de decisiones.",
     image: "/images/img_barber/dashboard_barber.gif", 
     tech: ["Power BI", "SQL Server"],
     github: "https://github.com/adison-chire/barberia-data-analytics.git",
@@ -37,5 +37,26 @@ export const projects = [
       "/images/img_barber/d_5.png"
     ],
     category: "Data Analyst"
+  },
+  {
+    id: 3,
+    title: "Sistema de Reservas de barberia",
+    description: "El sistema permite a los clientes registrarse, iniciar sesión y reservar citas, mientras que los administradores pueden gestionar servicios, controlar citas y mantener actualizada la información de la barbería.El objetivo principal es digitalizar el proceso de atención, mejorando la organización y brindando una mejor experiencia a clientes y barberos.",
+    image: "/images/img_web_barber/video_app_web_barberia.gif", 
+    tech: [" HTML", "CSS", "JavaScript","PHP 8", "MySQL"],
+    github: "https://github.com/adison-chire/reservas-barbershop.git",
+    // Agrega el array de media con el GIF y las imágenes
+    media: [
+      "/images/img_barber/dashboard_barber.gif", 
+      "/images/img_barber/sqlserver.png", 
+      "/images/img_barber/dimen.png", 
+      "/images/img_barber/data.png", 
+      "/images/img_barber/principal.png", 
+      "/images/img_barber/d_2.png", 
+      "/images/img_barber/d_3.png", 
+      "/images/img_barber/d_4.png", 
+      "/images/img_barber/d_5.png"
+    ],
+    category: "Apps Web"
   },
 ];
